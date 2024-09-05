f500d88cbc7fb2c8e98c9386fb7bef3df7f13913
pradip5474 <pradipmali5474@gmail.com>
2024-09-05 21:24:26 +0530

    Added routes for user

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -7,5 +7,6 @@ const router = Router();
 router.post('/signup', userController.signup);
 router.post('/signin', userController.signin);
 router.get('/users', userController.users);
+router.get('/users/:id', userController.usersid);
 
 export default router;

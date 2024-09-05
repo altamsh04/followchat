de7b877261051b0dff038071b8f5e548dfa316fd
altamsh <bairagdaraltamsh@gmail.com>
2024-09-05 23:10:51 +0530

    Updated image from schemas

--- server/db/user.js
+++ server/db/user.js
@@ -10,10 +10,8 @@ const userschema = new mongoose.Schema({
     required:true
   },
   image:{
-    type:String,
-    set: (v) =>
-      v === "" || v=== undefined  ? "https://imgs.search.brave.com/TPIEZzbY2bLFiuu9xwnM_EsR_f44cJtHWilFIjBsov0/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9jNC53/YWxscGFwZXJmbGFy/ZS5jb20vd2FsbHBh/cGVyLzk2OC81Mjkv/Mjg1L2l6enktbHVz/aC1tb2RlbC13b21l/bi1wb3Juc3Rhci13/YWxscGFwZXItcHJl/dmlldy5qcGc":v,        
-},
+    type:String
+  },
   password: {
     type: String,
     required: true,
